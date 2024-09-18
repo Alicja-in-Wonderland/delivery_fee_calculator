ebee3aed4e4fdc763e19bceb1f232856d67b0f9c
Alicja <alanotowska@yahoo.co.uk>
2024-09-18 21:10:46 +0200

    Adds function that takes a cart value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+
+    let delivery_cost = get_cart_value();
+    println!("The cost of your delivery is: {}", delivery_cost);
+
+}
+
+pub fn get_cart_value() -> u32 {
+    println!("Type the value of your order here:");
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer).unwrap();
+
+    // To change - cart_value should be of type float
+    let cart_value: u32 = buffer.trim().parse().unwrap();
+
+    const BASE_FEE: u32 = 1;
+    let delivery_cost: u32;
+
+    if cart_value >= 200 {
+            delivery_cost = 0;
+    } else if cart_value < 10 {
+            delivery_cost = BASE_FEE + (10 - cart_value);
+    } else {
+            delivery_cost = BASE_FEE;
+    }
+
+    delivery_cost
+
 }
