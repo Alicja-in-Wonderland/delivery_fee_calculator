06217c1a1fa4041a72503db8c34616021a661f5f
Alicja <alanotowska@yahoo.co.uk>
2024-09-20 15:44:00 +0200

    New project draft

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 use std::io::stdin;
 
 fn main() {
-    // The value returned from get_cart_value is stored in delivery_cost variable
+    // dear program,
+
+    // tell user what the fuck you do.
+    tell_user_what_you_do();
+
+    // obtain the value of the order from the user,
+    let order_value: f32 = obtain_order_value();
+    // obtain the delivery distance in meters from the user,
+    let distance_meters: u32 = obtain_distance();
+    // obtain the number of separate articles (pieces, items) their order consists of,
+    let number_of_articles: u32 = obtain_number_of_aricles();
+    // obtain the day of the week of the delivery,
+    let day_of_the_week: u32 = obtain_day_of_the_week();
+    // obtain the minute of the day of the delivery.
+    let minute_of_the_day: u32 = minute_of_the_day();
+
+    // calculate the delivery cost, according to specification.
+    let delivery_cost_cents: u32 = calculate_delivery_cost(
+        order_value,
+        distance_meters,
+        number_of_articles,
+        day_of_the_week,
+        minute_of_the_day,
+    );
+
+    // inform the user how much poorer they're going to be (assess the damage to their wallet).
+    inform_about_damage(delivery_cost);
+
+    // best regards,
+    // programmer
+}
+
+fn _main_old() {
     let delivery_cost = calculate_delivery_cost();
     println!("The cost of your delivery is: {} €", delivery_cost);
 }
@@ -23,26 +55,35 @@ pub fn calculate_delivery_cost() -> f32 {
     // then the parse() function converts the string slice into an f32.
     let cart_value: f32 = buffer.trim().parse().unwrap();
 
+    println!("Type the delivery distance (in meters):");
+    buffer.clear();
+    stdin().read_line(&mut buffer).unwrap();
+    let distance: f32 = buffer.trim().parse().unwrap();
+
     println!("Type the number of items in your cart:");
     buffer.clear();
     stdin().read_line(&mut buffer).unwrap();
     let number_of_items: f32 = buffer.trim().parse().unwrap();
 
-    const BASE_FEE: f32 = 1.0;
     let mut delivery_cost: f32;
 
-    // Checks different conditions to calculate the delivery cost based on cart/order value
+    // Calculates the delivery cost based on cart/order value
     if cart_value < 10.0 {
-        delivery_cost = BASE_FEE + (10.0 - cart_value);
+        delivery_cost = 10.0 - cart_value;
     } else {
-        delivery_cost = BASE_FEE;
+        delivery_cost = 0.0;
+    }
+
+    // Adjusts delivery cost for distance
+    if distance <= 500.0 {
+        delivery_cost += 1.0;
     }
 
     // Adjusts delivery cost based on the number of items
     if (5.0..=12.0).contains(&number_of_items) {
         delivery_cost += 0.50 * (number_of_items - 4.0);
     } else if number_of_items > 12.0 {
-        delivery_cost = delivery_cost + 0.50 * (number_of_items - 4.0) + 1.2;
+        delivery_cost += 0.50 * (number_of_items - 4.0) + 1.2;
     }
 
     if cart_value >= 200.0 {
@@ -50,5 +91,4 @@ pub fn calculate_delivery_cost() -> f32 {
     }
 
     delivery_cost
-
 }
